55f1f7c60e02aa8c1df2921e51d3d5f56be9f772
Igartuk <74913046+Igartuk@users.noreply.github.com>
2020-12-24 16:20:18 +0300

    init: working programm

---
+++ src/DateHandler/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DateHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите дату и мы узнаем день недели за вас: ");
+
+                var dateString = Console.ReadLine();
+
+                if (dateString.Equals("end", StringComparison.InvariantCultureIgnoreCase)) break;
+
+                var dt = DateTime.TryParse(dateString, out var result) ? result : (DateTime?)null;
+
+                if (dt == null)
+                {
+                    Console.WriteLine("Неверный формат,введите еще раз");
+                    continue;
+                }
+
+                Console.WriteLine(dt.Value.DayOfWeek);
+                Console.WriteLine("Если вы закончили,наберите end ");
+               
+            }
+        }
+    }
+}
